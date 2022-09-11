ad8e096ead84fb95e5f47c0d3f95a7da33aae507
heshiyu <450979309@qq.com>
2022-09-11 15:47:51 +0800

    feat: 项目配置

--- config/index.js
+++ config/index.js
@@ -1,8 +1,9 @@
 const config = {
   projectName: 'vip-mini',
   date: '2022-9-11',
-  designWidth: 750,
+  designWidth: 414,
   deviceRatio: {
+    414: 1.81,
     640: 2.34 / 2,
     750: 1,
     828: 1.81 / 2
