44e1d9d6a522b7fdbc260e6b66ed40df83266949
heshiyu <450979309@qq.com>
2022-09-19 22:20:25 +0800

    feat: 会员卡充值 60%

--- src/pages/recharge/index.tsx
+++ src/pages/recharge/index.tsx
@@ -23,9 +23,46 @@ export default class PageRecharge extends Component {
         </View>
 
         <View className='price-wrapper'>
-          <View className='item'>10000元</View>
+          <View className='item active'>10000元</View>
           <View className='item'>20000元</View>
           <View className='item'>30000元</View>
+        </View>
+
+        <View className='right-wrapper'>
+          <View className='item'>
+            <View className='icon'></View>
+            <View className='label'>赠送优惠券</View>
+            <View className='desc'>5张</View>
+          </View>
+          <View className='item'>
+            <View className='icon'></View>
+            <View className='label'>赠送优惠券</View>
+            <View className='desc'>5张</View>
+          </View>
+          <View className='item'>
+            <View className='icon'></View>
+            <View className='label'>赠送优惠券</View>
+            <View className='desc'>5张</View>
+          </View>
+          <View className='item'>
+            <View className='icon'></View>
+            <View className='label'>赠送优惠券</View>
+            <View className='desc'>5张</View>
+          </View>
+          <View className='item'>
+            <View className='icon'></View>
+            <View className='label'>赠送优惠券</View>
+            <View className='desc'>5张</View>
+          </View>
+        </View>
+
+        <View className='footer'>
+          <View className='btn-recharge'>立即充值</View>
+
+          <View className='notice'>
+            <View>说明:</View>
+            <View>1. 线上充值暂支持 10000元、20000元、50000元 三类面额，若单笔大于5万（不含）请前往线下门店</View>
+          </View>
         </View>
       </View>
     );
