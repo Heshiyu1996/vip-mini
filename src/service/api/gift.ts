5f39210ca600f48cfd10c1fd19a5658958f6a369
heshiyu <450979309@qq.com>
2023-06-03 17:34:51 +0800

    feat: 积分商城（签到已调通，欠礼品列表）

--- src/service/api/gift.ts
+++ src/service/api/gift.ts
@@ -1,8 +1,16 @@
-import { get } from '@/service/http';
+import { get, post } from '@/service/http';
 
+// 获得当前积分余额
+export const getPointBalance = () => get(`/api/point/balance`);
+
+// 获取积分商城签到状态
+export const getCheckInStatus = () => get(`/api/point/mall/checkIn/status`);
 
 // 积分收入明细
 export const getPointIncomeList = (params) => get(`/api/point/income/list`, params);
 
 // 积分支出明细
 export const getPointConsumptionList = (params) => get(`/api/point/consumption/list`, params);
+
+// 商城签到
+export const pointCheckIn = () => post(`/api/point/mall/checkIn`);
--- src/service/api/reward.ts
+++ src/service/api/reward.ts
@@ -5,3 +5,9 @@ export const getRewardBalance = () => get(`/api/user/reward/balance`);
 
 // 提现奖励金
 export const withdrawReward = (params) => post(`/api/user/reward/withdraw`, params);
+
+// 获得奖励金收入明细
+export const getRewardIncomeList = (params) => get(`/api/user/reward/income/list`, params);
+
+// 获得奖励金提现明细
+export const getRewardWithdrawList = (params) => get(`/api/user/reward/withdraw/list`, params);
